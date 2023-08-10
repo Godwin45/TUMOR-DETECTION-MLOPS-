b99c8532d4f861aa8bcc2ef2a50f67db50811f2e
GODWIN KING <71969710+Godwin45@users.noreply.github.com>
2023-08-10 23:52:04 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify, render_template
 import os
+
 from flask_cors import CORS, cross_origin
 from cnnClassifier.utils.common import decodeImage
 from cnnClassifier.pipeline.predict import PredictionPipeline
